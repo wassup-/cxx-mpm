349f905639082d6be27be9092b8cefa31a60a47d
Tom Knapen <tom_knapen@student.uhasselt.be>
2014-11-06 22:38:14 +0100

    testing of initial constexpr sin

--- example.cpp
+++ example.cpp
@@ -1,6 +1,5 @@
 #include "variables.hpp"
 #include "expressions.hpp"
-#include "inverse_expression.hpp"
 
 #include <iostream>
 
@@ -9,40 +8,9 @@ using mpm::variables::b;
 using mpm::variables::c;
 using mpm::variables::_;
 
-template<typename Expression, char Name>
-constexpr typename mpm::inverse_expression<Name, Expression>::type solve(Expression expr, mpm::Variable<Name> var)
-{
-  return { { } };
-}
-
-template<typename T>
-void print_type(T)
-{
-  std::cout << __PRETTY_FUNCTION__ << std::endl;
-}
-
-template<typename Expression, typename Result>
-void print_result(Expression expr, Result res, std::true_type)
-{
-  std::cout << expr.name() << " = " << res;
-}
-
-template<typename Expression, typename Result>
-void print_result(Expression expr, Result res, std::false_type)
-{
-  std::cout << expr << " = " << res;
-}
-
-template<typename Expression, typename Result>
-void print_result(Expression expr, Result res)
-{
-  print_result(expr, res, std::false_type{});
-}
-
 int main(int, char**)
 {
-  constexpr auto expr = (a = b + c);
-  constexpr auto res = expr((b = 4), (c = 1));
-  print_result(expr, res);
-  std::cout << std::endl;
+  constexpr auto expr = sin(a);
+  constexpr auto res = expr((a = 1));
+  std::cout << make_expression_printer(expr) << " = " << res << std::endl;
 }
