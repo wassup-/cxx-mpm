ecc01b711629d405f05a7a6faa7015c7506526af
Tom Knapen <tom_knapen@student.uhasselt.be>
2015-02-17 23:56:35 +0100

    added _t alias for easy type access

--- type_traits.hpp
+++ type_traits.hpp
@@ -4,18 +4,21 @@
 #include <type_traits>
 
 template<typename T>
+using _t = typename T::type;
+
+template<typename T>
 using not_ = std::integral_constant<bool, !T::value>;
 
 template<typename T, typename U>
 using enable_if = std::enable_if<T::value, U>;
 
 template<typename T, typename U = void>
-using enable_if_t = typename enable_if<T, U>::type;
+using enable_if_t = _t<enable_if<T, U>>;
 
 template<typename T, typename True, typename False>
 using conditional = std::conditional<T::value, True, False>;
 
 template<typename T, typename True, typename False>
-using conditional_t = typename conditional<T, True, False>::type;
+using conditional_t = _t<conditional<T, True, False>>;
 
 #endif
